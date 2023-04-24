a0f6c1c363ff23c52e4e2c2e162644db7d35c37b
Adrià Batlle <sudobat@gmail.com>
2023-04-24 12:41:49 +0200

    Initial commit

---
+++ src/main/java/com/chesscuacho/kasparov/KasparovApplication.java
@@ -0,0 +1,13 @@
+package com.chesscuacho.kasparov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KasparovApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KasparovApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chesscuacho/kasparov/KasparovApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chesscuacho.kasparov;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KasparovApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
