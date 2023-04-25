5b116184beae2ac374c26f6830eb09e8617495b5
ejavierip@yahoo.es <ejavierip@yahoo.es>
2023-04-25 11:44:49 +0200

    push con error

--- src/test/java/com/chesscuacho/kasparov/evaluator/impl/MaterialHeuristicEvaluatorTests.java
+++ src/test/java/com/chesscuacho/kasparov/evaluator/impl/MaterialHeuristicEvaluatorTests.java
@@ -142,8 +142,6 @@ public class MaterialHeuristicEvaluatorTests {
 
         MaterialHeuristicEvaluator oM = new MaterialHeuristicEvaluator();
         int evaluation = oM.evaluate(initTable);
-        //assertThat(evaluation).isEqualTo(+3900);
-        assertThat(evaluation).isEqualTo(-3900);
-
+        assertThat(evaluation).isEqualTo(+123900);
     }
 }
