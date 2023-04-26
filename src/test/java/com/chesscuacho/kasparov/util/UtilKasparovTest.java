2cc51f0e5227c26630873da9684f6f348a1c025c
emtobias <emtobias@larioja.org>
2023-04-26 13:09:14 +0200

    Integration other heuristic's implementations

---
+++ src/test/java/com/chesscuacho/kasparov/util/UtilKasparovTest.java
@@ -0,0 +1,48 @@
+package com.chesscuacho.kasparov.util;
+
+import com.chesscuacho.kasparov.evaluator.impl.BishopHeuristicEvaluator;
+import com.chesscuacho.kasparov.evaluator.util.UtilKasparov;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class UtilKasparovTest {
+
+    @Test
+    void initFenIsInitMatrix() {
+
+        String[][] initMatrixTable = {
+                {"r", "n", "b", "q", "k", "b", "n", "r"},
+                {"p", "p", "p", "p", "p", "p", "p", "p"},
+                {" ", " ", " ", " ", " ", " ", " ", " "},
+                {" ", " ", " ", " ", " ", " ", " ", " "},
+                {" ", " ", " ", " ", " ", " ", " ", " "},
+                {" ", " ", " ", " ", " ", " ", " ", " "},
+                {"P", "P", "P", "P", "P", "P", "P", "P"},
+                {"R", "N", "B", "Q", "K", "B", "N", "R"}
+        };
+        String initFen = UtilKasparov.INIT_BOARD;
+        assertThat(UtilKasparov.FEN2Matrix(initFen)).isEqualTo(initMatrixTable);
+
+    }
+
+    @Test
+    void initFenIsValid() {
+
+
+        String initFen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR";
+        assertThat(UtilKasparov.isValidFen(initFen));
+
+    }
+
+    @Test
+    void invalidFenIsNotValid() {
+
+        String initFen = "rnbqkbnr/pApppppp/8/8/8/8/PPPPPPPP/RNBQKBNR";
+        assertThat(UtilKasparov.isValidFen(initFen)).isEqualTo(false);
+
+    }
+
+
+
+}
