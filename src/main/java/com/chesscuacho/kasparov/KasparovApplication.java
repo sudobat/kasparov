931604660b0cbeada4f93f47bbbe94458017f5d3
emtobias <emtobias@larioja.org>
2023-04-28 10:01:33 +0200

    Integration other heuristic's implementations

--- src/main/java/com/chesscuacho/kasparov/KasparovApplication.java
+++ src/main/java/com/chesscuacho/kasparov/KasparovApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class KasparovApplication {
 
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(KasparovApplication.class, args);
 	}
